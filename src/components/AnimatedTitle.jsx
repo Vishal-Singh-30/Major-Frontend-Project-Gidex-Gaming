5473220161be2c1aa6ac73631b2a3263134df273
Vishal Singh <mailtovishalsingh30@gmail.com>
2025-03-17 13:45:10 +0530

    About Section Completed With GSAP Animation

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AnimatedTitle = () => {
+  return (
+    <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>Disc<b>o</b>ver the w<b>o</b>rld's <br /> l<b>a</b>rgest sha<b>r</b>ed ad<b>v</b>enture</div>
+  )
+}
+
+export default AnimatedTitle
